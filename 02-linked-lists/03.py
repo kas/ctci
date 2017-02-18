387c25d1796ec76181ea3b429b8c126b744b8e93
Kenneth Schnall <kas6570@psu.edu>
2017-02-17 23:32:19 -0500

    Finish problem 2-03

---
+++ 02-linked-lists/03.py
@@ -0,0 +1,27 @@
+# implement an algorithm to delete a node in the middle of a singly linked list, given only access to that node
+
+# implemented with help from https://stackoverflow.com/questions/69209/deleting-a-middle-node-from-a-single-linked-list-when-pointer-to-the-previous-no
+
+from singlylinkedlist import SinglyLinkedList
+
+def delete_node(singly, node):
+    node.data = node.next_node.data
+    next_node = node.next_node.next_node
+    del node.next_node
+    node.next_node = next_node
+    return singly
+
+singly = SinglyLinkedList()
+singly.append('a')
+singly.append(56)
+singly.append('b')
+singly.append('c')
+singly.append('d')
+singly.append('a')
+singly.append('a')
+singly.print()
+
+print('\ndeleting node...\n')
+
+singly = delete_node(singly, singly.head.next_node.next_node) # node is 'b'
+singly.print()
