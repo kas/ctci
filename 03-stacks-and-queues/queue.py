974567ac26f0224c55bb6eef73db269d05ed75d7
Kenneth Schnall <kas6570@psu.edu>
2017-02-20 22:06:33 -0500

    Add stack and queue data structures for chapter 3

---
+++ 03-stacks-and-queues/queue.py
@@ -0,0 +1,31 @@
+class Queue:
+    '''Queue'''
+
+    def __init__(self):
+        '''Initialize queue'''
+
+        self.items = []
+
+    def enqueue(self, data):
+        '''Append data to queue'''
+        self.items.insert(0, data)
+
+    def dequeue(self):
+        '''Remove last item from queue'''
+        return self.items.pop()
+
+    def print(self):
+        '''Print linked list'''
+
+        print(self.items)
+
+# queue = Queue()
+# queue.enqueue('First')
+# queue.enqueue('Second')
+# queue.enqueue('Third')
+# queue.enqueue('Fourth')
+# queue.print()
+
+# print('\ndequeueing')
+# print(queue.dequeue())
+# queue.print()
---
+++ 03-stacks-and-queues/stack.py
@@ -0,0 +1,60 @@
+class Node:
+    '''Node with data and next_node'''
+
+    def __init__(self, data, next_node=None):
+        '''Initialize the Node'''
+
+        self.data = data
+        self.next_node = next_node
+
+    def __str__(self):
+        return str(self.data)
+
+class Stack:
+    '''Stack'''
+
+    def __init__(self):
+        '''Initialize stack'''
+
+        self.top = None
+
+    def pop(self):
+        '''Pop value off top of stack'''
+        self.top = self.top.next_node
+
+        return self.top
+
+    def push(self, data):
+        '''Push value to top of stack'''
+
+        t = self.top
+        self.top = Node(data)
+        self.top.next_node = t
+
+    def peek(self):
+        '''Return top node'''
+        if not self.top:
+            return None
+        return self.top.data
+
+    def print(self):
+        '''Print linked list'''
+
+        if not self.top:
+            print(None)
+        else:
+            itr_nd = self.top
+            print(itr_nd)
+            while itr_nd.next_node:
+                itr_nd = itr_nd.next_node
+                print(itr_nd)
+
+# stack = Stack()
+# stack.push('bottom')
+# stack.push('middle')
+# stack.push('top')
+# stack.print()
+
+# print('\npopping!')
+# stack.pop()
+# stack.print()
