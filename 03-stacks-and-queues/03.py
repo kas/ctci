9e7ddd9f3cf6f31e8626deff0d8d3ca3dffa46d0
Kenneth Schnall <kas6570@psu.edu>
2017-02-24 22:23:14 -0500

    Finish problem 3-03 part 2

--- 03-stacks-and-queues/03.py
+++ 03-stacks-and-queues/03.py
@@ -28,6 +28,30 @@ class SetOfStacks:
 
         return data
 
+    def pop_at(self, s_i):
+        '''Pop value off top of specified stack in setofstacks'''
+
+        # check if popping off stack[99999] will crash the program
+
+        if (len(self.stacks) - 1) < s_i:
+            raise IndexError('pop from empty stack in setofstacks')
+
+        data = self.stacks[s_i][0]
+        del self.stacks[s_i][0]
+
+        for i, stack in enumerate(self.stacks):
+            if i < s_i:
+                continue
+
+            if (len(stack) < self.limit) and ((len(self.stacks) - 1) >= (i + 1)):
+                stack.append(self.stacks[i + 1][len(self.stacks[i + 1]) - 1])
+                del self.stacks[i + 1][len(self.stacks[i + 1]) - 1]
+
+                if self.stacks[i + 1] == []:
+                    del self.stacks[i + 1]
+
+        return data
+
     def push(self, data):
         '''Push value to top of setofstacks'''
 
@@ -52,9 +76,19 @@ class SetOfStacks:
 # set_of_stacks.push('Second pushed')
 # set_of_stacks.push('Third pushed')
 # set_of_stacks.push('Fourth pushed')
+# set_of_stacks.push('Fifth pushed')
+# set_of_stacks.push('Sixth pushed')
+# set_of_stacks.push('Seventh pushed')
+# set_of_stacks.push('Eighth pushed')
+# set_of_stacks.push('Ninth pushed')
+# set_of_stacks.push('Tenth pushed')
 # set_of_stacks.print()
 
 # print('\npopping:')
 # set_of_stacks.pop()
 # set_of_stacks.pop()
+# set_of_stacks.print()
+
+# print('\npopping from stack at index 1:')
+# set_of_stacks.pop_at(1)
 # set_of_stacks.print()
